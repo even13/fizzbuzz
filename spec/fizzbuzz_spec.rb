c2114ecd1c1f65fbe5e3086362b908eba0f6333a
Eve <eve.noirault@live.fr>
2019-11-08 13:32:53 +0000

    tests all run

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,18 @@ describe 'fizzbuzz' do
     it 'returns "fizz" when passed 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
     end
+    
+    it 'returns "buzz" when passed 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+    
+    it 'returns "fizzbuzz" when passed 15' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+    
+    it 'returns number when passed any other number' do
+        expect(fizzbuzz(8)).to eq '8'
+    end
+    
+    
 end
